919635ad703df4ab2043ea03e5623bd88bd678d9
sebastien charton <sebastiencharton@protonmail.com>
2023-10-17 22:25:41 +0300

    useless class removed

--- src/Infrastructure/Options/PasswordConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Auth.Api.Infrastructure.Options;
-
-public class PasswordConfiguration
-{
-    public int SaltSize { get; set; }
-    public int Iterations { get; set; }
-    public int HashSize { get; set; }
-}
